87573084f9a92b39361c86e42c5cedf1c18f1cd3
老贾 <jxfsuda@users.noreply.github.com>
2022-05-26 12:55:25 +0800

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,16 @@
+uniapp vite插件
+用于 uniapp编译成功后,往dist目录复制文件
+
+let node_env = process.env.NODE_ENV;
+
+
+export default defineConfig({
+	plugins: [ 
+		uni(),  
+		vitePluginUniCopy([{
+			 node_env, // production or other
+			 source: ["/components/bui-vr-preview/g3d.js","/common/config.js"] ,  //多个文件的数组
+			 rootDir:   process.env.UNI_INPUT_DIR    // 项目根目录
+		}])
+	]
+});
---
+++ vite.uni-copy.js
@@ -0,0 +1,67 @@
+/**
+ *   初衷是复制 g3d.js  因为这个库不是umd规范,uni貌似不会追踪到他
+ * 
+	实现了 uni编译完成后 ,复制某些文件到最终目录中去. 
+	有点类似 ant 或 maven的copy_resource
+ 
+ */ 
+var fs = require('fs');
+var path = require('path');
+  
+
+export default (options)=> {
+	var name = 'vite-plugin-copy-uniapp_config';
+
+	return {
+		name: name,
+		enforce: 'post', 
+		closeBundle:()=>{  //buildEnd之后运行
+			options.forEach(function(option) {
+					// console.log("closeBundle 触发");
+					var node_env = option.node_env;
+					var source = option.source;
+					
+					let env_name="dev";
+					if(node_env=="production"){
+						env_name="build";
+					} 
+					let rootDir = option.rootDir
+					let destDir = rootDir+"/unpackage/dist/"+ env_name +"/mp-weixin/";
+					
+					try {
+						 
+						 for(let f of source){
+							 let ff = rootDir+ f
+							 let df = destDir+ f
+							 
+							 let destPath = path.dirname(df)
+							 if(!fs.existsSync(destPath)){  //多级目录下,如果目录没有被uni编译,那么认为组件没有被使用
+							 	//fs.mkdirSync(destDir);
+							 	console.log(destPath+" 没有创建,认为组件没有被使用,略过");  //这里的逻辑可以修改,甚至可以配置未是否强制覆盖等
+								continue;
+							 }
+							 fs.readFile(ff,(err,data)=>{
+							     if(!err){//读取成功  
+							         fs.writeFile(df,data,(err)=>{
+							             if(!err){
+							                 console.log(f+" 拷贝成功");
+							             }else{
+							                 console.log(fs+" 拷贝失败",err);
+							             }
+							         })
+							     }else{
+									 console.error(f+" 读取错误,无法复制")
+								 }
+							 }) 
+							 
+						 }
+						 
+					} catch (err) {
+						throw new Error(err);
+					}
+					 
+			});
+		}
+		
+	};
+} 
